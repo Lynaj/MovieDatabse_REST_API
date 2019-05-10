f47fdd2ab716d7b74e5382d5ebad6626a359a0f0
203200 Drozdzyk Artur <203200@edu.p.lodz.pl>
2019-05-10 14:51:14 +0200

    Added followig applications: movies & comments

--- backend/config/api.py
+++ backend/config/api.py
@@ -1,9 +1,12 @@
 from rest_framework import routers
-from apps.users.views import *
+
+from apps.movies.views import *
+from apps.comments.views import *
 
 # Settings
 api = routers.DefaultRouter()
 api.trailing_slash = '/?'
 
 # Users API
-api.register(r'users', UserViewSet, base_name="Users Types View")
+api.register(r'movies', MovieViewSet, base_name="Movies View")
+api.register(r'comments', CommentViewSet, base_name="Comments View")
